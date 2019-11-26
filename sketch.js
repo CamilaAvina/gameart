55e78bcb28e731bfab1b34475308f8fec1ff6f95
Camila Avina <twin2avina@gmail.com>
2019-11-25 23:17:10 -0800

    trying to work on project

--- sketch.js
+++ sketch.js
@@ -1,78 +1,63 @@
-let button;
+'use strict';
+// let bird;
+let state = 'title';
 let cnv;
-let yourInputs = [];
-let submit = [];
-let count = 11;
 
-function setup() {
-  cnv = createCanvas(500, 500);
-  cnv.parent("#canvasDiv");
-   //background(255);
+function setup(){
+cnv= createCanvas (600,600);
 
-   button = select('#Button');
-   button.mousePressed(buttonPressed);
-   button.class("Btn");
-
-
-
-for(let i = 0; i <5; i++) {
-
-   yourInputs.push( createInput());
-   yourInputs[yourInputs.length-1].parent("inputFields");
-   //yourInputs.position(100,100);
+ // bird = new bird();
 }
 
-}
-function draw() {
-  noStroke();
-//  fill('pink');
-  rect(225, 130, 250, 150);
-  textSize(100);
-   text(count,width/2, height/2);
+function draw(){
+  if (state==='title'){
+    title();
+    cnv.mouseClicked(titleMouseClicked);
 
-
-
-
-}
-function buttonPressed(){
-for (let i = 0; i < yourInputs.length; i++) {
-  submit.push(yourInputs[i].value());
-
-}
-count= count+11;
+  }
+  else if (state === 'level 1'){
+    level1();
+    cnv.mouseClicked(level1MousedClicked);
+  }
 }
 
 
+function title(){
+background(0);
+textSize(100);
+stroke(255);
+text('Welcome',100,100);
+// bird.update();
+// bird.show();
+textSize(30);
+text('click anywhere to start',100,300);
+}
+
+function titleMouseClicked(){
+  state = 'level 1';
+}
+
+
+function level1(){
+  background( 50,150,200);
+}
+
+function level1MousedClicked(){
+
+}
+function mousePressed(){
+  state = 'level 1';
+
+}
 
 
 
 
 
+function keyPressed() {
+  if (key == ''){
+   //  bird.up();
+   // // console.log("space");
 
-
-
-
-
-
-
-
-
-// let checkboxes = [];
-// let size=100;
-// function setup() {
-//   createCanvas(500, 500);
-//    background(0);
-//    for (let i = 0; i <100; i ++ ){
-//   checkboxes[i] = createCheckbox('label');
-//
-//   checkboxes[i].style('display','inline');
-//   checkboxes[i].position(random(windowWidth)),(random(windowHeight));
-//   checkboxes[i].changed(myCheckedEvent);
-// }
-// }
-// function myCheckedEvent();
-//
-// function draw() {
-//   ellispe(mouseX,mouseY,size);
-//
-// }
+  }
+}
