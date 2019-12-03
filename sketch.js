e00040e21762d2218b700ce04338321239273a8e
Camila Avina <twin2avina@gmail.com>
2019-12-03 15:22:03 -0800

    new files

--- sketch.js
+++ sketch.js
@@ -1,86 +1,43 @@
-let bg;
-let index;
-let state = 'title';
-let cnv;
 var ship;
 var pipes = [];
+
 function setup() {
-  bg1 = loadImage('assets/Shatterdearth.png');
-  bg = loadImage('assets/space.png');
-  img = loadImage('assets/spaceship.png');
+  createCanvas(windowWidth, windowHeight);
 
-  createCanvas(800, 400);
-  ship = new Ship()
   pipes.push(new Pipe());
-
-
-
 }
 
 function draw() {
+  background(0);
 
-
-   if (state==='title'){
-    title();
-    //cnv.mouseClicked(titleMouseClicked);
-
-  }
-  else if (state === 'level 1'){
-    level1();
-    //cnv.mouseClicked(level1MousedClicked);
-  }
-
-    for (var i = pipes.length-1; i >= 0; i--) {
+  for (var i = pipes.length - 1; i >= 0; i--) {
     pipes[i].show();
     pipes[i].update();
 
-      if (pipes[i].hits(ship)) {
-      }
-      if (pipes[i].offscreen()){
+    if (pipes[i].hits(ship)) {
+
+    }
+
+    if (pipes[i].offscreen()) {
       pipes.splice(i, 1);
     }
+
+
   }
 
   ship.update();
+  ship.show();
 
-  if (frameCount % 100 == 0 ){
+  if (frameCount % 100 == 0) {
     pipes.push(new Pipe());
   }
 
+
 }
 
-function keyPressed(){
-  if (key == ' '){
+function keyPressed() {
+  if (key == ' ') {
     ship.up();
   }
-
-}
-
-function title(){
-background(bg1);
-textSize(100);
-stroke(255);
-text('Welcome',100,100);
-// bird.update();
-// bird.show();
-textSize(30);
-text('click anywhere to start',100,300);
-}
-
-function titleMouseClicked(){
-  state = 'level 1';
-}
-
-function level1(){
-background(bg);
-    ship.show();
-
-}
-
-function level1MousedClicked(){
-}
-
-function mousePressed(){
-  state = 'level 1';
 
 }
