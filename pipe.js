b05427911dfce13f5c59f67d32424e9030cd8e14
zainiba <47758673+zainiba@users.noreply.github.com>
2019-12-03 17:54:21 -0800

    orignal working pipe

--- pipe.js
+++ pipe.js
@@ -1,16 +1,9 @@
 function Pipe() {
   debris = [nes, car, trash];
-  this.top = random(height / 2.5);
-  this.bottom = random(height / 2.5);
+  this.y = random(height);
+  //this.bottom = random(height / 2.5);
   var deb = debris[Math.floor(Math.random() * debris.length)];
-<<<<<<< HEAD
-  console.log(deb);
-  //this.debris = random(imageArray)
-  // this.r = this.image.width
-  // instead of top and bottom you will need this.y and this.r
-=======
 
->>>>>>> 1d06e09ff52f3e13fbfbb2741686c09a6d02c75a
   this.x = width;
   this.w = 20;
   this.speed = 2;
@@ -18,14 +11,6 @@ function Pipe() {
   this.highlight = false;
 
   this.hits = function(ship) {
-<<<<<<< HEAD
-    if (ship.y < this.top || ship.y > height - this.bottom) {
-      if (ship.x > this.x && ship.x < this.x + 20) {
-        this.highlight = true;
-        return true;
-      }
-    }
-=======
     if (dist(ship.x, ship.y, this.x, this.y) < 30) {
       if (ship.x < 200){
 
@@ -34,7 +19,6 @@ function Pipe() {
     }}
 
 
->>>>>>> 1d06e09ff52f3e13fbfbb2741686c09a6d02c75a
     this.highlight = false;
     return false;
   }
@@ -44,26 +28,12 @@ function Pipe() {
     if (this.highlight) {
       fill(255, 0, 0);
       noLoop();
-
-
-    button.mousePressed(title);
-    }
-
-<<<<<<< HEAD
-    rect(this.x, 0, this.w, this.top)
-    rect(this.x, height-this.bottom, this.w, this.bottom)
-
+      button.show();
 
     }
 
-
-    image(deb, this.x, this.top, 60,60);
-    image(deb, this.x, height - this.bottom, 60,60);
-
-=======
     image(deb, this.x, this.y, 60, 60);
     //  image(deb, this.x, height - this.bottom, 60,60);
->>>>>>> 1d06e09ff52f3e13fbfbb2741686c09a6d02c75a
   }
 
   this.update = function() {
