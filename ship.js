7b697e04019c165c84c886561e29dab4a5502df7
zainiba <47758673+zainiba@users.noreply.github.com>
2019-12-05 17:18:39 -0800

    larks help fixes

--- ship.js
+++ ship.js
@@ -1,37 +1,38 @@
 let img;
 
-function Ship(){
-  this.y = height/2;
+function Ship() {
+  this.y = height / 2;
   this.x = 100;
 
   this.gravity = .4;
-  this.lift=-15;
+  this.lift = -15;
   this.velocity = 0;
 
-  this.show = function(){
-     image(img,this.x,this.y, 60,40);
+  this.show = function() {
+    image(img, this.x, this.y, 60, 40);
   }
 
-  this.up = function(){
+  this.up = function() {
     this.velocity += this.lift;
     console.log(this.velocity);
   }
 
-  this.update = function(){
+  this.update = function() {
     this.velocity += this.gravity;
     this.velocity *= 0.9;
     this.y += this.velocity;
 
-  if (this.y > height){
-    this.y = height;
+    if (this.y > height-60) {
+      console.log("hit bottom")
+      this.y = height-60;
       this.velocity = 0;
-  }
+    }
 
-  this.y += this.velocity;
-    if (this.y < 0){
-    this.y = 0;
+    if (this.y < 0) {
+      this.y = 0;
       this.velocity = 0;
-  }
+    }
+    this.y += this.velocity;
 
   }
 
--- sketch.js
+++ sketch.js
@@ -15,18 +15,18 @@ function setup() {
   car = loadImage('assets/sketch.png');
   trash = loadImage('assets/trash.png');
 
-button = createButton('GAMEOVER');
+  button = createButton('GAMEOVER');
 
-button.mousePressed(title);
-button.size(100,50);
-button.style("background-color","#fc0000");
-button.style("color","#fff");
+  button.mousePressed(title);
+  button.size(100, 50);
+  button.style("background-color", "#fc0000");
+  button.style("color", "#fff");
 
 
-  createCanvas(1100, 400);
-  button.position( 550, 350);
+  cnv = createCanvas(1100, 400);
+  button.position(550, 350);
   ship = new Ship()
-  pipes.push(new Pipe());
+  //pipes.push(new Pipe());
 
 
 
@@ -35,69 +35,54 @@ button.style("color","#fff");
 function draw() {
 
 
-   if (state==='title'){
+  if (state === 'title') {
     title();
-    //cnv.mouseClicked(titleMouseClicked);
+    cnv.mouseClicked(titleMouseClicked);
 
-  }
-  else if (state === 'level 1'){
+  } else if (state === 'level 1') {
     level1();
-    //cnv.mouseClicked(level1MousedClicked);
-  }
-
-
-
-  ship.update();
-
-  if (frameCount % 100 == 0 ){
-    pipes.push(new Pipe());
+    cnv.mouseClicked(level1MousedClicked);
   }
 
 }
 
-function keyPressed(){
-  if (key == ' '){
+function keyPressed() {
+  if (key == ' ') {
     ship.up();
   }
 
 }
 
-function title(){
-button.hide();
-background(bg1);
-// textSize(100);
-// stroke(255);
-//text('Welcome',100,100);
-// bird.update();
-// bird.show();
-//textSize(30);
-//text('click anywhere to start',100,300);
+function title() {
+  button.hide();
+  background(bg1);
 }
 
-function titleMouseClicked(){
+function titleMouseClicked() {
   state = 'level 1';
 }
 
-function level1(){
-background(bg);
-    ship.show();
-    for (var i = pipes.length-1; i >= 0; i--) {
+function level1() {
+  background(bg);
+
+  ship.show();
+  ship.update();
+
+  if (frameCount % 100 == 0) {
+    pipes.push(new Pipe());
+    console.log("pushed pipe");
+  }
+
+  for (var i = pipes.length - 1; i >= 0; i--) {
     pipes[i].show();
     pipes[i].update();
 
-      if (pipes[i].hits(ship)) {
-      }
-      if (pipes[i].offscreen()){
+    if (pipes[i].hits(ship)) {}
+    if (pipes[i].offscreen()) {
       pipes.splice(i, 1);
     }
   }
 
 }
 
-function level1MousedClicked(){
-}
-
-function mousePressed(){
-  state = 'level 1';
-
-}
+function level1MousedClicked() {}
